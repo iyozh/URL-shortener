9489e137f790b0248c177656cd2095d20b4ebef4
iyozh <evzhenko1106@gmail.com>
2020-10-21 12:16:30 +0300

    stats page upgrade

--- src/applications/statistics/views/statistics.py
+++ src/applications/statistics/views/statistics.py
@@ -1,8 +1,16 @@
 from django.views.generic import DetailView
 
 from applications.homepage.models import Link
+from applications.statistics.models import Hit
 
 
 class StatisticsView(DetailView):
     template_name = "statistics/statistics.html"
     model = Link
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        hits = Hit.objects.filter(url_id=self.object.id)
+
+        ctx.update({"hits":hits})
+        return ctx
