1c21446fc980f8f7eef4613d18749ed27f52b480
iyozh <evzhenko1106@gmail.com>
2020-11-07 20:05:44 +0300

    new test and run selenium

--- tests/functional/test_app_homepage.py
+++ tests/functional/test_app_homepage.py
@@ -1,5 +1,9 @@
 import pytest
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 from tests.functional.pages import HomePage
 
@@ -12,6 +16,22 @@ def test_get(browser):
 
     validate_title(page)
     validate_structure(page)
+    validate_content(page, "URL Shortener")
+
+
+def test_post(browser):
+    link = "http://example.com"
+
+    page = HomePage(browser, url)
+
+    validate_title(page)
+    validate_structure(page)
+    validate_content(page, "URL Shortener")
+
+    enter_link(page, link)
+    submit(page)
+
+    validate_redirect(page)
     validate_content(page, "URL Shortener")
 
 
@@ -32,3 +52,21 @@ def validate_structure(page: HomePage):
 def validate_content(page: HomePage, content):
     html = page.html
     assert content in html
+
+
+def enter_link(page: HomePage, link):
+    page.input_link.clear()
+    if link:
+        page.input_link.send_keys(link)
+
+
+def submit(page: HomePage):
+    page.button_shorten.send_keys(Keys.RETURN)
+
+
+def validate_redirect(page: HomePage):
+    try:
+        redirect = WebDriverWait(page.browser, 4).until(expected_conditions.url_matches(url))
+        assert redirect
+    except TimeoutException:
+        raise AssertionError("no redirect")
