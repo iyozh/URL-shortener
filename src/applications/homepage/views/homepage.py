e1fe7e341ce95341044fa8aaaefa5ac4e5cfa33b
iyozh <evzhenko1106@gmail.com>
2020-11-23 19:46:02 +0300

    adding qrcode

--- src/applications/homepage/views/homepage.py
+++ src/applications/homepage/views/homepage.py
@@ -1,11 +1,15 @@
 import secrets
+from io import BytesIO
 
+import boto3
+import pyqrcode
 from django.urls import reverse_lazy
 from django.views.generic import FormView
 
 from applications.homepage.forms import UrlInputForm
 from applications.homepage.models import Link
-from applications.statistics.models import UTM
+from applications.statistics.models import UTM, QRCode
+from dynaconf import settings as _ds
 
 
 class HomePageView(FormView):
@@ -34,8 +38,20 @@ class HomePageView(FormView):
             utm_copy=original,
         )
         url.save()
+        url_id = url.id
 
-        utm = UTM(link_id=url.id)
+        qr_code = pyqrcode.create(shortcut)
+        buffer = BytesIO()
+        qr_code.png(buffer, scale=8)
+        buffer.seek(0)
+
+        s3 = boto3.client("s3")
+        s3.put_object(Body=buffer, Bucket='urlcutt', Key=f'{_ds.AWS_S3_CODES_LOCATION}/code-{url_id}.png', ACL="public-read")
+
+        qr = QRCode(original=f'{_ds.AWS_S3_CODES_LOCATION}/code-{url_id}.png', link_id=url_id)
+        qr.save()
+
+        utm = UTM(link_id=url_id)
         utm.save()
 
         return super().form_valid(form)
