b5a94fb1fd85b295344f7acc45e82bf4d9179a3f
iyozh <evzhenko1106@gmail.com>
2021-08-03 19:50:11 +0200

    implemented pagination on stats page using ajax

--- src/applications/statistics/views/statistics.py
+++ src/applications/statistics/views/statistics.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator
+from django.http import JsonResponse
 from django.views.generic import DetailView
 from django.views.generic.edit import FormMixin
 
@@ -13,13 +15,23 @@ class StatisticsView(FormMixin, DetailView):
     model = Link
     form_class = CheckboxForm
 
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        page_num = self.request.POST.get("page_n")
+        result = list(context.get("paginator").page(page_num).object_list.values("ip_address", "location", "time",
+                                                                                 "browser", "os"))
+        return JsonResponse({"result": result})
+
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         hits = Hit.objects.filter(link_id=self.object.id)
         initial_values = _get_utm_initial_values(self.object)
         utm = UtmForm(initial=initial_values)
-        ctx.update({"hits": hits, "utm": utm})
+        paginator = Paginator(hits, 5)
+        ctx.update({"hits": hits, "utm": utm, "paginator": paginator})
         return ctx
 
     def get_initial(self):
         return {Link.confirm.field.name: self.object.confirm}
+
